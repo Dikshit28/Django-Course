78bed452d2f2d0f299e59853a617ff2bcc8fba8c
Dikshit <dikshit19csu379@ncuindia.edu>
2021-07-08 12:49:13 +0530

    changes my_blog to my_site

--- my_site/my_site/urls.py
+++ my_site/my_site/urls.py
@@ -1,4 +1,4 @@
-"""blog_app URL Configuration
+"""my_site URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
